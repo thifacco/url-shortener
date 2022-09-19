5a559822e33a66197293df7f5c0f35d558c352ea
Tiago Facco <thifacco@gmail.com>
2022-09-19 16:52:57 -0300

    Custom alphabet

--- src/urls/shared/urls.service.ts
+++ src/urls/shared/urls.service.ts
@@ -3,7 +3,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUrlDto } from '../dto/create-url.dto';
 import { Url } from './url';
-import { nanoid } from 'nanoid'
+import { customAlphabet } from 'nanoid'
+
+const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwyxz', 5)
 
 @Injectable()
 export class UrlsService {
@@ -12,11 +14,19 @@ export class UrlsService {
     @InjectModel('Url') private readonly urlModel: Model<Url>
   ) {}
 
+  async findAll() {
+    return await this.urlModel.find().exec();
+  }
+
+  async findOne(id: string) {
+    return await this.urlModel.findById(id).exec();
+  }
+
   async create(createUrlDto: CreateUrlDto) {
 
     const hashCode = nanoid();
 
-    const urlObj: Url = {
+    const newUrl: Url = {
       longUrl: createUrlDto.longUrl,
       urlCode: hashCode,
       shortUrl: process.env.URL_APP + hashCode,
@@ -24,17 +34,9 @@ export class UrlsService {
       expirationDate: new Date()
     }
 
-    const createdShortUrl = new this.urlModel(urlObj);
+    const createdShortUrl = new this.urlModel(newUrl);
     await createdShortUrl.save();
 
     return 'This action adds a new url';
-  }
-
-  async findAll() {
-    return await this.urlModel.find().exec();
-  }
-
-  async findOne(id: string) {
-    return await this.urlModel.findById(id).exec();
   }
 }
